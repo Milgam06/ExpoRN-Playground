05c67b990b5fed1ccaff04c0fd50a6d7aa5802ef
MILGAM <93873127+Milgam06@users.noreply.github.com>
2025-10-21 23:51:14 +0900

    feat: add RNGH SwipeableRef

--- playground/swipeable/components/rngh/rngh.component.tsx
+++ playground/swipeable/components/rngh/rngh.component.tsx
@@ -1,6 +1,6 @@
-import { memo } from 'react';
+import { memo, useCallback, useRef } from 'react';
 import { Text, View } from 'react-native';
-import Swipeable from 'react-native-gesture-handler/ReanimatedSwipeable';
+import Swipeable, { SwipeableMethods } from 'react-native-gesture-handler/ReanimatedSwipeable';
 import Reanimated, { SharedValue, useAnimatedStyle } from 'react-native-reanimated';
 
 type IRNGHSwipeableComponentProps = {
@@ -8,13 +8,23 @@ type IRNGHSwipeableComponentProps = {
 };
 
 export const RNGHSwipeableComponent = memo<IRNGHSwipeableComponentProps>(({ childrenContainerWidth }) => {
+  const SwipeableRef = useRef<SwipeableMethods>(null);
+
+  const closeSwipeable = useCallback(() => {
+    if (!SwipeableRef.current) {
+      return;
+    }
+    SwipeableRef.current.close();
+  }, []);
   const RightAction = memo(({ drag }: { drag: SharedValue<number> }) => {
     const styleAnimation = useAnimatedStyle(() => ({
       transform: [{ translateX: drag.value + childrenContainerWidth }],
     }));
 
     return (
-      <Reanimated.View style={[styleAnimation, { width: childrenContainerWidth, paddingHorizontal: 4 }]}>
+      <Reanimated.View
+        style={[styleAnimation, { width: childrenContainerWidth, paddingHorizontal: 4 }]}
+        onTouchStart={closeSwipeable}>
         <Text style={{ backgroundColor: 'purple', height: '100%', borderRadius: 8, padding: 10 }}>Text</Text>
       </Reanimated.View>
     );
@@ -22,6 +32,7 @@ export const RNGHSwipeableComponent = memo<IRNGHSwipeableComponentProps>(({ chil
 
   return (
     <Swipeable
+      ref={SwipeableRef}
       containerStyle={{ width: '100%', paddingHorizontal: 4 }}
       friction={1}
       rightThreshold={childrenContainerWidth}
